5667569923b8bb258be54a1300aa9a34544dcb10
Fer Contreras DSG <fcontreras@dsgcorp.com.mx>
2024-10-06 01:06:45 +0200

    modal error envio carta instruccion

--- Components-React/src/views/CartaInstruccion.jsx
+++ Components-React/src/views/CartaInstruccion.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
-import { IconPDF } from '../components/Icons';
+import { IconPDF,IconErrorModal } from '../components/Icons';
 import useEnvioInstruction from '../services/useFetchSendInstruccionPago';
 
 function CartaInstruccion() {
     const [file, setFile] = useState(null);
     const [isFileLoaded, setIsFileLoaded] = useState(false);
     const sendInstruccionPago = useEnvioInstruction();
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
 
     const handleFileChange = (event) => {
         const selectedFile = event.target.files[0];
@@ -33,39 +42,63 @@ function CartaInstruccion() {
     };
 
     return (
-        <div className="card">
-            <span className="card-enc"><b>Envío de Carta Instrucción</b></span>
-            <svg width="100%" height="2" viewBox="0 0 1093 2" fill="none">
-                <path d="M0 1H1093" stroke="#007AFF" strokeWidth="2" />
-            </svg>
-            <div className="card-content">
-                <div className='cardHorizontal'>
-                    <div className="container__load__file">
-                        <p> Carga tu documento de Carta Instrucción.</p>
-                        <input 
-                            type="file" 
-                            id="subirArchivoPDF" 
-                            accept=".pdf" 
-                            onChange={handleFileChange} 
-                        />
-                        <label 
-                            htmlFor="subirArchivoPDF" 
-                            className={`uploadComponent ${isFileLoaded ? '--uploadFile' : ''}`}
-                        >
-                            <IconPDF />
-                            <span>Carga tu archivo .pdf</span>
-                        </label>
-                        <button 
-                            id='SendFile' 
-                            className='btn --btn-azul' 
-                            onClick={handleSendFile}
-                        >
-                            Enviar
-                        </button>
+        <>
+
+
+            <div className="card">
+                <span className="card-enc"><b>Envío de Carta Instrucción</b></span>
+                <svg width="100%" height="2" viewBox="0 0 1093 2" fill="none">
+                    <path d="M0 1H1093" stroke="#007AFF" strokeWidth="2" />
+                </svg>
+                <div className="card-content">
+                    <div className='cardHorizontal'>
+                        <div className="container__load__file">
+                            <p> Carga tu documento de Carta Instrucción.</p>
+                            <input
+                                type="file"
+                                id="subirArchivoPDF"
+                                accept=".pdf"
+                                onChange={handleFileChange}
+                            />
+                            <label
+                                htmlFor="subirArchivoPDF"
+                                className={`uploadComponent ${isFileLoaded ? '--uploadFile' : ''}`}
+                            >
+                                <IconPDF />
+                                <span>Carga tu archivo .pdf</span>
+                            </label>
+                            <button
+                                id='SendFile'
+                                className='btn --btn-azul'
+                                onClick={handleSendFile}
+                            >
+                                Enviar
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            {isModalOpen && (
+                <div className="modal">
+                    <div className="modal-content --sm-modal">
+                        <div className='modal-content-head ---end-align'>
+                            <button type='button' className="close-button" onClick={closeModal}>&times;</button>
+
+                        </div>
+                            <div className="container__content__ModalErr">
+                                    <IconErrorModal/>
+                                    <span className="msjError">
+                                    Error al subir el documento
+                                    </span>
+                                    <span className="detailError">Formato de archivo no admitido, tu archivo debe ser en formato .pdf</span>
+                            </div>
+                    </div>
+                </div>
+             )} 
+        </>
+
+
+
     );
 }
 
