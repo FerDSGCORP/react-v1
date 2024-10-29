e29a6bd40f8d77cb32b16da4dd4a4e094a5e2bba
Fer Contreras DSG <fcontreras@dsgcorp.com.mx>
2024-10-30 00:41:05 +0100

    preparativo front Reportar problema

--- Components-React/src/components/reportarProblema.jsx
+++ Components-React/src/components/reportarProblema.jsx
@@ -1,71 +1,123 @@
 import React, { useState } from "react";
+import { IconPicture, IconPictureDelete } from "./Icons";
 
 function ReportarProblema() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [uploadedFiles, setUploadedFiles] = useState([]);
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const openModal = () => {
-        setIsModalOpen(true);
-    };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
 
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
-    return (
-        <>
-            <div className="problema">
-                <div className="problema-card"  onClick={openModal}>
-                    <span><b>¿Tienes un problema?</b></span>
-                    <span><b>¡Podemos ayudarte!<br /><br />Escríbenos</b></span>
-                </div>
-                <div className="problema-img"></div>
+  const handleFileChange = (event) => {
+    const files = Array.from(event.target.files);
+    const imageFiles = files.filter((file) => file.type.startsWith("image/"));
+    setUploadedFiles([...uploadedFiles, ...imageFiles]);
+  };
+
+  const handleDeleteFile = (index) => {
+    const newFiles = [...uploadedFiles];
+    newFiles.splice(index, 1);
+    setUploadedFiles(newFiles);
+  };
+
+  const truncateFileName = (name) => {
+    return name.length > 8 ? name.substring(0, 8) + "..." : name;
+  };
+
+  return (
+    <>
+      <div className="problema">
+        <div className="problema-card" onClick={openModal}>
+          <span>
+            <b>¿Tienes un problema?</b>
+          </span>
+          <span>
+            <b>
+              ¡Podemos ayudarte!
+              <br />
+              <br />
+              Escríbenos
+            </b>
+          </span>
+        </div>
+        <div className="problema-img"></div>
+      </div>
+
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content --sm-modal __modal100">
+            <div className="modal-content-head ---end-align --border-none">
+              <button type="button" className="close-button" onClick={closeModal}>
+                &times;
+              </button>
             </div>
-
-            {isModalOpen && (
-                <div className="modal">
-                    <div className="modal-content --sm-modal __modal100">
-                        <div className='modal-content-head ---end-align --border-none'>
-                            <button type='button' className="close-button" onClick={closeModal}>&times;</button>
-
-                        </div>
-                        <div className="bannerTitle">
-                            <p>Describe tu problema en el siguiente formulario y a la brevedad le daremos una respuesta / solución al problema que nos reportas</p>
-                        </div>
-                        <div className="container__content">
-                            <form className="frm_reportar" action="">
-                                <div className="container__field">
-                                    <span>Reporte del problema:</span>
-                                    <select name="selectProblema" id="selectProblema">
-                                        <option value="" selected>Selecciona una opción</option>
-                                        <option value="Conexion">Conexión</option>
-                                        <option value="Subida de archivo">Subida de archivo</option>
-                                        <option value="Pantalla no visible">Pantalla no visible</option>
-                                        <option value="Falta de informacion">Falta de información</option>
-                                        <option value="Información errónea">Información errónea</option>
-                                        <option value="Error">Error</option>
-                                    </select>
-                                </div>
-                                <div className="container__field">
-                                    <span>Descripción del problema:</span>
-                                    <textarea name="" id="" rows="5"></textarea>
-                                </div>
-                                <div className="container__field">
-                                    <input type="file" name="" id="uploadFileProblem" />
-                                    <label htmlFor="uploadFileProblem" className="btn --btn-azul --btn-sm" id="btn-upload-file-problem">Seleccionar archivo</label>
-                                    <span>En caso de no tener imagen del error dejar este campo vacío</span>
-                                </div>
-                                <div className="container__field container__btn">
-                                    <button className="btn --btn-azul">Enviar</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+            <div className="bannerTitle">
+              <p>
+                Describe tu problema en el siguiente formulario y a la brevedad le daremos una respuesta / solución al
+                problema que nos reportas
+              </p>
+            </div>
+            <div className="container__content">
+              <form className="frm_reportar" action="">
+                <div className="container__field">
+                  <span>Reporte del problema:</span>
+                  <select name="selectProblema" id="selectProblema">
+                    <option value="" selected>
+                      Selecciona una opción
+                    </option>
+                    <option value="Conexion">Conexión</option>
+                    <option value="Subida de archivo">Subida de archivo</option>
+                    <option value="Pantalla no visible">Pantalla no visible</option>
+                    <option value="Falta de informacion">Falta de información</option>
+                    <option value="Información errónea">Información errónea</option>
+                    <option value="Error">Error</option>
+                  </select>
                 </div>
-
-            )}
-        </>
-
-    );
+                <div className="container__field">
+                  <span>Descripción del problema:</span>
+                  <textarea name="" id="" rows="5"></textarea>
+                </div>
+                <div className="container__field">
+                  <input
+                    type="file"
+                    id="uploadFileProblem"
+                    multiple
+                    accept="image/*"
+                    onChange={handleFileChange}
+                  />
+                  <label htmlFor="uploadFileProblem" className="btn --btn-azul --btn-sm" id="btn-upload-file-problem">
+                    Seleccionar archivo
+                  </label>
+                  <div className="filesloaded">
+                    {uploadedFiles.map((file, index) => (
+                      <div className="fileCharge" key={index}>
+                        <i className="deleteItem --ml-auto" onClick={() => handleDeleteFile(index)}>
+                          <IconPictureDelete />
+                        </i>
+                        <i>
+                          <IconPicture />
+                        </i>
+                        <span>{truncateFileName(file.name)}</span>
+                      </div>
+                    ))}
+                  </div>
+                  <span>En caso de no tener imagen del error dejar este campo vacío</span>
+                </div>
+                <div className="container__field container__btn">
+                  <button className="btn --btn-azul">Enviar</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default ReportarProblema
+export default ReportarProblema;
